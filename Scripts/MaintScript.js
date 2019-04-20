a21f6186b6ae19c75925deff4f0482eaf8d3025c
DaemonWhite <DaemonWhite@users.noreply.github.com>
2019-04-20 16:38:44 +0200

    Test2

--- Scripts/MaintScript.js
+++ Scripts/MaintScript.js
@@ -1,6 +1,9 @@
-Function countToTen(){
+function countToTen(){
 	var count =0;
-	While (count < 10)
+	while (count < 10){
 	count ++;
-	document.getElementById("theCount").innerHTML += count + "<br>";
+	if (count==1) {document.getElementById("theCount").innerHTML +="Le monde va mieux mon cher " + "et de " + count + "<br>";}
+	else if (count==2) {document.getElementById("theCount").innerHTML +="C'est pour sa que je vous sauverai " + "et de " + count + "<br>";} 
+	
+	}
 }
