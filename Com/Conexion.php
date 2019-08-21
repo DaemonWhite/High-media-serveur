aeb70ad568ed94666764f005f0f284f37b385e23
DaemonWhite <44682988+DaemonWhite@users.noreply.github.com>
2019-08-21 22:10:38 +0200

    Add files via upload

---
+++ Com/Conexion.php
@@ -0,0 +1,95 @@
+<?php 
+session_start();
+
+if (!empty($_GET['invite'])) {
+  
+    $_SESSION['ID'] = "-1";
+    $_SESSION['Pseudo'] = "Anonyme";
+    $_SESSION['Securiter'] = "-1";
+
+    header("Location: " . $moveUrl);
+
+}
+
+$ip_Visiteur = $_SERVER['REMOTE_ADDR'];
+
+$bdd = new pdo('mysql:host=localhost;dbname=highmediadata', 'root','',   array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+if (isset($_POST['DemandeConexion'])) {
+
+    $PseudConnect = htmlspecialchars($_POST['PseudConnect']);
+
+    if (!empty($PseudConnect)) {
+
+        if (!empty($_POST['PassConnect'])) {
+
+            $requser = $bdd->prepare("SELECT * FROM membre WHERE UserName = ?");
+            $requser->execute(array($PseudConnect));
+            $UserInfo = $requser->fetch();
+
+            $UserInfo['ID_user'];
+
+            
+
+            if (password_verify($_POST['PassConnect'], $UserInfo['Password'])) {
+
+                
+
+                $_SESSION['ID'] = $UserInfo['ID_user'];
+                $_SESSION['Pseudo'] = $UserInfo['UserName'] ;
+                $_SESSION['Securiter'] = $UserInfo['Securiter'];
+
+                if (!empty($_POST['SaveMe'])) {
+
+                    if (($_SERVER['REMOTE_ADDR'] = $UserInfo['IP1']) OR ($_SERVER['REMOTE_ADDR'] = $UserInfo['IP2'])) {
+    
+                                header("Location: Global.php");
+                        # code...
+                    } else {
+
+                        if ($UserInfo['Modif_IP'] == 0) {
+    
+                                $mod = 1;
+                                $_SERVER['REMOTE_ADDR'];
+                                $UpdateIp = $bdd->prepare('UPDATE membre SET IP1 = :ip, Modif_IP = :modif WHERE ID_user = :ID ');
+                                $UpdateIp->execute(array(
+                                    'ip' => $ip_Visiteur,
+                                    'modif' => $mod,
+                                    'ID' => $_SESSION['ID']
+                                    ));
+    
+                                header("Location: Global.php");
+
+
+    
+                            } else { 
+    
+                                $mod = 0;
+    
+                                $UpdateIp = $bdd->prepare('UPDATE membre SET IP2 = :IP2, Modif_IP = :modif WHERE ID_user = :ID ');
+                                $UpdateIp->execute(array(
+                                    'IP2' => $ip_Visiteur,
+                                    'modif' => $mod,
+                                    'ID' => $UserInfo['ID_user']
+                                    ));  header("Location: Global.php");
+                                    echo "Choix D"; 
+                            }
+
+                    }
+
+                    setcookie('IP', $_SERVER['REMOTE_ADDR'], time() + 365*24*3600, null, null, false, true);
+
+                    setcookie('ID', $UserInfo['ID_user'], time() + 365*24*3600, null, null, false, true);
+                    setcookie('Pseudo', $UserInfo['UserName'], time() + 365*24*3600, null, null, false, true);
+                    setcookie('Securiter', $UserInfo['Securiter'], time() + 365*24*3600, null, null, false, true);
+                }
+                # code...
+            } else {$erreur = "Identifiant ou mot de passe incorecte";}
+            # code...
+        } else {$erreur = "Veulier mettre votre mot de passe";}
+        # code...
+    } else {$erreur = "Veulier entrer un nom d'utilisateur";}
+    # code...
+}
+
+?>
---
+++ Com/verifiLoad.php
@@ -0,0 +1,60 @@
+<?php
+if (!empty($_GET['Deco'])) {
+
+    session_destroy();
+    setcookie("ID");
+    setcookie("Pseudo");
+    setcookie("Securiter");
+    setcookie("IP");
+
+    echo "cookie suprimer";
+
+    session_start();
+
+    $_SESSION['Pseudo'] = "Anonyme";
+    $_SESSION['ID'] = "-1";
+    $_SESSION['Securiter'] = "-1";
+
+    echo "Deco";
+
+    header('Location: global.php');
+}
+
+if (empty($_SESSION['ID']) OR empty($_SESSION['Pseudo'] OR empty($_SESSION['Securiter']))) {
+
+    echo "vide";
+
+    if (empty($_COOKIE['ID']) OR empty($_COOKIE['Pseudo']) OR empty($_COOKIE['Securiter'])) {
+        echo "vide2";
+        header('Location: index.php');
+
+    } else {
+            $_SESSION['ID'] = $_COOKIE['ID'];
+            $_SESSION['Pseudo'] = $_COOKIE['Pseudo'];
+            $_SESSION['Securiter'] = $_COOKIE['Securiter'];
+        }
+    } 
+
+
+
+    if ($_SESSION['Securiter'] == 1) {
+
+        $typeUser = "Utilisateur";
+        # code...
+    } elseif ($_SESSION['Securiter'] == 2) {
+
+        $typeUser = "Modérateur";
+        # code...
+    } elseif ($_SESSION['Securiter'] == 3) {
+
+        $typeUser = "Administrateur";
+        # code...
+    } elseif ($_SESSION['Securiter'] === "-1") {
+
+        $typeUser = "Inviter";
+        $_SESSION['Pseudo'] = "Anonyme";
+
+    }
+    $Pseudo = $_SESSION['Pseudo'];
+
+?>
