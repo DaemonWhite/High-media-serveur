63d0d111a1ea6c01e8aaaace1df0b42506ba038f
DaemonWhite <44682988+DaemonWhite@users.noreply.github.com>
2020-01-31 14:57:02 +0000

    Papa

--- lang/FR.php
+++ lang/FR.php
@@ -1,5 +1,54 @@
 <?php
 
 
+# Generique
+
+# --- Menu
+	$_Lang_Gen_Homme = "Accueille";
+	$_Lang_Gen_Audio = "Audio";
+	$_Lang_Gen_Video = "Vidéo";
+	$_Lang_Gen_Perso = "Espace Personnelle";
+	$_Lang_Gen_Serve = "Serveur";
+	$_Lang_Gen_Propo = "Proposition";
+
+	$_Lang_Gen_Upload 	= "Upload";
+	$_Lang_Gen_Fichi	= "Gérer mes fichier";
+	$_Lang_Gen_Down 	= "Télèchrger";
+	$_Lang_Gen_Favoris 	= "Favoris";
+
+	$_Lang_Gen_Conex = "Connexion";
+	$_Lang_Gen_Deco  = "Deconexopn";
+
+	
+# Accueille
+
+	$_Lang_ACC_Nmus = "Nouvelle musique";
+	$_Lang_Acc_Nvid = "Nouvelle vidéo";
+	$_Lang_Acc_Hist = "Historique";
+	$_Lang_Acc_Info = "Information";
+
+	$_Lang_Acc_Admn = "Admin";
+	$_Lang_Acc_Modo = "modérateur";
+	$_Lang_Acc_Norm = "Utilisateur";
+	$_Lang_Acc_Invi = "Inviter" ;
+
+
+# Connexion
+
+
+
+# Video
+
+
+
+# Audio
+
+
+
+# Parametre
+
+
+# Lecteur Video
+
 
 ?>
